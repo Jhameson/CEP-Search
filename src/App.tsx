4f7a8c184803c6f2aa61dc91a4b306aa46249f8f
jhameson <jhamesonlucas22@gmail.com>
2022-04-22 22:20:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
